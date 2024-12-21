ed7cb1baba2a539f3906af1d57258d324f4ce2a4
Ligg <156616301+liggmxa2003@users.noreply.github.com>
2024-12-21 21:57:10 +0800

    优化

--- src/main/java/com/ligg/pojo/ChatMessage.java
+++ src/main/java/com/ligg/pojo/ChatMessage.java
@@ -1,6 +1,7 @@
 package com.ligg.pojo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,13 +12,14 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ChatMessage {
-    private Long id;                  // 消息ID
-    private String fromUser;          // 发送者用户名
-    private String toUser;            // 接收者用户名
-    private String content;           // 消息内容
+    private Long id;// 消息ID
+    private String fromUser;// 发送者用户名
+    private String toUser;// 接收者用户名
+    @Pattern(regexp = "[a-z A-Z0-9]{1,150}")
+    private String content;// 消息内容
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime sendTime;   // 发送时间
-    private Boolean isRead;           // 是否已读
+    private LocalDateTime sendTime;// 发送时间
+    private Boolean isRead;// 是否已读
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 }
