dcf20b06078352171f20bf30822de1562ec6576e
Ligg <156616301+liggmxa2003@users.noreply.github.com>
2025-01-09 00:32:03 +0800

    关注列表

--- src/main/java/com/ligg/service/User/UserFollowService.java
+++ src/main/java/com/ligg/service/User/UserFollowService.java
@@ -1,6 +1,7 @@
 package com.ligg.service.User;
 
 import com.ligg.pojo.Result;
+import com.ligg.pojo.dto.UserFollowDto;
 import com.ligg.pojo.user.UserFollow;
 
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.List;
 public interface UserFollowService {
     Result<String> follow(Long id, Boolean isFollow);
     List<UserFollow> list(Long id);
+    // 关注列表
+    List<UserFollowDto> followList();
 }
