062e025d4e4f0e8b6d7cde196454f73f488d583d
Ligg <156616301+liggmxa2003@users.noreply.github.com>
2025-01-13 22:45:45 +0800

    用户数据列表

--- src/main/java/com/ligg/pojo/user/User.java
+++ src/main/java/com/ligg/pojo/user/User.java
@@ -20,7 +20,7 @@ public class User {
     private Long id;//主键ID
     private String username;//用户名
     @JsonIgnore//忽略
-    @Pattern(regexp = "")//2-10位非空字符
+    @Pattern(regexp = "^\\S{2,13}$")//2-13位非空字符
     private String password;//密码
     @NotEmpty(groups = User.Update.class)
     @Pattern(regexp = "^\\S{2,13}$")//2-10位非空字符
